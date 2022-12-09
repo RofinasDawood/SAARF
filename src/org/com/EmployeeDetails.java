0ce02de8e64323e07cb498fc5e599aaceaebd779
RofinaSDawood <rofinasdawood@gmail.com>
2022-12-09 10:03:03 +0530

    First

---
+++ src/org/com/EmployeeDetails.java
@@ -0,0 +1,23 @@
+package org.com;
+
+public class EmployeeDetails {
+	private void emp1() {
+		System.out.println("RofinaSheikDawood");
+
+	}
+	private void emp2() {
+		System.out.println("Ramesh");
+
+	}
+	private void emp3() {
+		System.out.println("SivaNathan");
+
+	}
+	private void emp4() {
+	System.out.println("Jagadeeshwaran");
+
+}
+	private void emp5() {
+		System.out.println("Loganadhan");
+
+} }
